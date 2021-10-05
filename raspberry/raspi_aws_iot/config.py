f6133a052b68e8a84ef97f5cf74d0de257c15232
matti16 <bmattia92@gmail.com>
2021-10-05 14:31:13 +0200

    cloudfront distribution

--- raspberry/raspi_aws_iot/config.py
+++ raspberry/raspi_aws_iot/config.py
@@ -1,13 +1,13 @@
 from uuid import uuid4
 
 class MQTTConfig:
-    TOPIC = "raspberry-aws-iot/dev"
+    TOPIC = "matt-iot/dev"
     PORT = 443
     ROOT_CA = "/home/pi/.aws/Amazon-root-CA-1.pem"
     CERT = "/home/pi/.aws/device.pem.crt"
     KEY = "/home/pi/.aws/private.pem.key"
     ENDPOINT = "a3ig8qh8u66gen-ats.iot.eu-west-1.amazonaws.com"
-    CLIENT_ID = "raspberry-" + str(uuid4())
+    CLIENT_ID = "matt-iot-" + str(uuid4())
 
 
 class DeviceConfig:
