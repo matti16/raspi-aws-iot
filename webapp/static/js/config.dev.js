007b1ac9d4ec21596a4a5f8a50797a37e263c575
matti16 <bmattia92@gmail.com>
2021-10-07 17:54:32 +0200

    new endpoint

--- webapp/static/js/config.dev.js
+++ webapp/static/js/config.dev.js
@@ -1,6 +1,6 @@
 const config = {
-    apiHost: "https://oh9vvnrlpb.execute-api.eu-west-1.amazonaws.com/dev/api",
+    apiHost: "https://gofgthony8.execute-api.eu-west-1.amazonaws.com/dev/api",
     authUrl: "https://matt-iot-pool-domain-dev.auth.eu-west-1.amazoncognito.com/login",
     clientID: "3r18t9657bdanc2bl0eio77l6a",
-    calbackUrl: "https://d1qhs0tssnuf8r.cloudfront.net"
+    calbackUrl: "https://d2cpl0x2418o5h.cloudfront.net"
 };
