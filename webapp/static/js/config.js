70fa64a6ed497a9ed62d9580b5c53cd72cfa6e17
matti16 <bmattia92@gmail.com>
2021-10-07 19:31:45 +0200

    client app id

--- webapp/static/js/config.js
+++ webapp/static/js/config.js
@@ -1,6 +1,6 @@
 const config = {
     apiHost: "https://gofgthony8.execute-api.eu-west-1.amazonaws.com/dev/api",
     authUrl: "https://matt-iot-pool-domain-dev.auth.eu-west-1.amazoncognito.com/login",
-    clientID: "3r18t9657bdanc2bl0eio77l6a",
+    clientID: "161egqfiqmjdajkn1fbs1idkof",
     calbackUrl: "https://d2cpl0x2418o5h.cloudfront.net"
 };
