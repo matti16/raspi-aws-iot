b8df89d6a88abc73a6e5855ccf65e199ed74db5f
matti16 <bmattia92@gmail.com>
2021-10-08 18:59:29 +0200

    fix login redicrect

--- webapp/static/js/config.dev.js
+++ webapp/static/js/config.dev.js
@@ -1,5 +1,5 @@
 const config = {
-    apiHost: "https://gofgthony8.execute-api.eu-west-1.amazonaws.com/dev/api",
+    apiHost: "https://e8s3ni67ik.execute-api.eu-west-1.amazonaws.com/dev/api",
     authUrl: "https://matt-iot-pool-domain-dev.auth.eu-west-1.amazoncognito.com/login",
     clientID: "42sn5ljmnpthioabj4bvp73gae",
     calbackUrl: "https://d14h7pm1cj3wjw.cloudfront.net"
